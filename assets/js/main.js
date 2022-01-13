5f100dde929bae4f1877cd9572d37c27c065d823
César Prado <cesarhenrique_prado@hotmail.com>
2022-01-13 00:07:58 -0300

    Correçoes Front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
-const card = document.querySelector('.card__inner');
+const front = document.querySelectorAll('.b_front');
+const back = document.querySelectorAll('.b_back');
 
-card.addEventListener('click', function() {
-    card.classList.toggle('is-flipped');
-});
+front.forEach(item => {
+    item.addEventListener('click', event => {
+        const ft = event.target.parentElement.parentElement;
+        ft.classList.toggle('flipped')
+        const btn_front = document.querySelector("#frt");
+        if(btn_front.style.pointerEvents === "none") {
+            btn_front.style.pointerEvents = "auto";
+        }else{
+            btn_front.style.pointerEvents = "none";
+        }
+    })
+})
+back.forEach(item => {
+    item.addEventListener('click', event => {
+        const bk = event.target.parentElement.parentElement.parentElement.parentElement;
+        bk.classList.toggle('flipped')
+        const btn_front = document.querySelector("#frt");
+        if(btn_front.style.pointerEvents === "auto") {
+            btn_front.style.pointerEvents = "none";
+        }else{
+            btn_front.style.pointerEvents = "auto";
+        }
+    })
+})
+
