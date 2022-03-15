b5ae6c1cf7b849a63968a6e3a82c97f32285049d
CesarPrado <cesarjunior@brasilcard.net>
2022-03-15 16:42:06 -0300

    Edit

--- AxiosApi/api.js
+++ AxiosApi/api.js
@@ -4,6 +4,8 @@ function getUser () {
     axios.get('http://localhost:5000/result')
         .then(response => {
             console.log(response.data.result_ok[0].English)
+            console.log(response.data.result_ok[0].Portuguese)
+            var pt = response.data.result_ok[0].Portuguese
             var ing = response.data.result_ok[0].English
             return    
         })
--- scraping.js
+++ scraping.js
@@ -1,14 +1,14 @@
 const puppeteer = require ('puppeteer');
 const fs = require('fs');
-const getUser = require('./AxiosApi/api');
+const api = require('./AxiosApi/api');
 
-console.log(getUser);
+console.log(api.getUser);
 
 
 (async () => {
     const browser = await puppeteer.launch();
     const pageGrammar = await browser.newPage();
-    const dictionaryEnglish = (`https://dictionary.cambridge.org/pt/dicionario/ingles/${getUser}`);
+    const dictionaryEnglish = (`https://dictionary.cambridge.org/pt/dicionario/ingles/${api.getUser}`);
     await pageGrammar.goto(dictionaryEnglish);
 
     const resultGrammar = await pageGrammar.evaluate(() => {
